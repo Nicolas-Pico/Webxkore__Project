cb32c6bb973358b46c2fc69578c40dcd91f867fb
IngJohoxtum <ing.johoxtumjimenez@gmail.com>
2024-10-08 00:02:57 -0500

    :sparkles: top productos

--- src/app/modules/public/public-routing.module.ts
+++ src/app/modules/public/public-routing.module.ts
@@ -6,6 +6,7 @@ import {RegisterComponent} from "./register/register.component";
 import {ContactComponent} from "./contact/contact.component";
 
 const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
